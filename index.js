ef261a47de91840c762305ca563152b94d2cfc01
NIKITA AD <nikita8ad@gmail.com>
2018-07-25 06:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,12 +67,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var emptyArray = [];
-  if (cardNumber === undefined) {
-    console.log('We don\'t have a credit card on file for you to place your order.')
-  }
-  else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)    
-    setCart(emptyArray)
-  }
+  if(cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    for (var i = cart.length; i>= 0; i--) {
+      cart.pop();
+    } 
+    } else 
+      console.log('Sorry, we don\'t have a credit card on file for you.')
+return cart;
 }
